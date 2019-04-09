931094dd83339c212c4265360fc50b5608bd31bd
Mario Franco <MFranc0@outlook.com>
2019-04-08 21:46:12 -0700

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+    
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Double Double Burger');
+INSERT INTO burgers (burger_name) VALUES ('Chicken Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Burger');
